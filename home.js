ef59270968baa20377b9ce41d53c584d48a9dde6
Joseph <serinojoseph@gmail.com>
2021-04-15 19:59:57 -0700

    initial commit

---
+++ home.js
@@ -0,0 +1,7 @@
+let input = document.getElementById("input")
+let button = document.getElementById("button")
+let paragraph = document.getElementById("paragraph")
+
+function showInput() {
+  paragraph.innerText = input.value;
+}
